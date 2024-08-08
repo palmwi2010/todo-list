a19929df7afb5aa8194d1191cf5ce85084949d40
palmwi2010 <wop3000@hotmail.co.uk>
2024-08-08 14:08:27 +0100

    Built sidebar

--- src/banner.js
+++ src/banner.js
@@ -1,10 +1,21 @@
 import {createElement} from './utils.js';
+import profileImg from './assets/profile_img.jpg';
 
 function render() {
 
-    let container = createElement({'type': 'div', 'id': 'top-banner'});
-    let header = createElement({'type': 'p', 'elemClass': 'profile-name', 'elemText': 'Will Palmer'});
-    container.appendChild(header);
+    let $container = createElement({'type': 'div', 'id': 'top-banner'});
+    let $header = createElement({'type': 'p', 'elemClass': 'profile-greet', 'elemText': 'Welcome,'});
+    let $greeting = createElement({'type': 'p', 'elemClass': 'profile-name', 'elemText': 'Bart52'});
+    let $profileImg = createElement({'type': 'img', 'elemClass': 'profile-img'});
+    let $logoutBtn = createElement({'type': 'button', 'elemClass': 'logut-btn', 'elemText': 'Logout'});
+
+    $profileImg.src = profileImg;
+    $profileImg.alt = 'User profile image';
+
+    $container.appendChild($header);
+    $container.appendChild($greeting);
+    $container.appendChild($profileImg);
+    $container.appendChild($logoutBtn);
 
     return container;
 }
--- src/sidebar.js
+++ src/sidebar.js
@@ -1,12 +1,63 @@
 import {createElement} from './utils.js';
+import allImg from './assets/all-inclusive-box.svg';
+import upcomingImg from './assets/forward.svg';
+import priorityImg from './assets/alert.svg';
+import completedImg from './assets/check-circle.svg';
+import categoryImg from './assets/folder.svg';
+import settingsImg from './assets/cog.svg';
+import privacyImg from './assets/book-alert.svg';
+import phoneImg from './assets/phone.svg';
+
+const menuItems = [
+    [
+        {'name': 'All items', 'img': allImg},
+        {'name': 'Upcoming items', 'img': upcomingImg},
+        {'name': 'High priority', 'img': priorityImg},
+        {'name': 'Completed', 'img': completedImg},
+        {'name': 'Categories', 'img': categoryImg}
+    ],
+    [
+        {'name': 'Settings', 'img': settingsImg},
+        {'name': 'Privacy policy', 'img': privacyImg},
+        {'name': 'Contact us', 'img': phoneImg}
+    ]
+]
+
+function createSidebarItem(menuItem) {
+    let container = createElement({'type': 'div', 'elemClass': 'menu-item'});
+    let menuLabel = createElement({'type': 'p', 'elemClass': 'menu-label', 'elemText': menuItem.name});
+    let menuImg = createElement({'type': 'img', 'elemClass': 'icon'});
+    menuImg.src = menuItem.img;
+    container.appendChild(menuImg);
+    container.appendChild(menuLabel);
+    return container;
+}
 
 function render() {
 
-    let container = createElement({'type': 'div', 'id': 'sidebar'});
-    let header = createElement({'type': 'h1', 'elemClass': 'sidebar-logo', 'elemText': 'Tododay'});
-    container.appendChild(header);
+    let $container = createElement({'type': 'div', 'id': 'sidebar'});
+    let $header = createElement({'type': 'h1', 'elemClass': 'sidebar-logo', 'elemText': 'Tododay'});
+    $container.appendChild($header);
 
-    return container;
+    // Sub menu 1
+    let $menu = createElement({'type': 'div', 'elemClass': 'menu'});
+    let $menuHeader = createElement({'type': 'h3', 'elemClass': 'menu-header', 'elemText': 'Dashboard'});
+    $menu.appendChild($menuHeader);
+
+    menuItems[0].forEach(item => $menu.appendChild(createSidebarItem(item)));
+
+    $container.appendChild($menu);
+
+    // Sub menu 2
+    $menu = createElement({'type': 'div', 'elemClass': 'menu'});
+    $menuHeader = createElement({'type': 'h3', 'elemClass': 'menu-header', 'elemText': 'Help'});
+    $menu.appendChild($menuHeader);
+
+    menuItems[1].forEach(item => $menu.appendChild(createSidebarItem(item)));
+
+    $container.appendChild($menu);
+
+    return $container;
 }
 
 export {render};
